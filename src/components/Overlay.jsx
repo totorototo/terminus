2e423b41d78cd693390c25f713b1694cf6f93ecc
toto rototo <el.totorototo@gmail.com>
2025-09-25 21:09:16 +0200

    display locations

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,6 +1,11 @@
 import { useSpring as useSpringWeb, animated } from "@react-spring/web";
 
 function Overlay({ section }) {
+  const { opacity } = useSpringWeb({
+    opacity: section ? 1 : 0,
+    config: { tension: 170, friction: 26 },
+  });
+
   const { distance } = useSpringWeb({
     distance: section?.totalDistance || 0,
     config: { tension: 170, friction: 26 },
@@ -51,6 +56,12 @@ function Overlay({ section }) {
       </h1>
       {section && (
         <>
+          <animated.div style={{ opacity }}>
+            {section.startLocation && section.endLocation
+              ? `From: ${section.startLocation} → To: ${section.endLocation}`
+              : "Location data not available"}
+          </animated.div>
+
           <animated.div>
             {distance.to((n) => `Distance: ${(n / 1000).toFixed(2)} km`)}
           </animated.div>
@@ -60,11 +71,16 @@ function Overlay({ section }) {
           <animated.div>
             {elevationLoss.to((n) => `Elevation Loss: ${n.toFixed(0)} m`)}
           </animated.div>
-          <div
-            style={{ marginTop: "1em", fontSize: "0.8em", color: "#707070" }}
+          <animated.div
+            style={{
+              marginTop: "1em",
+              fontSize: "0.8em",
+              color: "#707070",
+              opacity,
+            }}
           >
             Click on another section to see its details.
-          </div>
+          </animated.div>
         </>
       )}
     </div>
