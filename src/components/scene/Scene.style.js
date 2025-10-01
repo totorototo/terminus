a5d5c69ac2f07240a04d4f0fa3f9b8699d00f9b1
toto rototo <el.totorototo@gmail.com>
2025-10-01 14:27:00 +0200

    update ceheckpoint style

--- src/components/scene/Scene.style.js
+++ src/components/scene/Scene.style.js
@@ -2,18 +2,18 @@ import styled from "styled-components";
 
 const style = (Component) => styled(Component)`
   .checkpoint-label {
-    background: rgba(82, 81, 81, 0.93);
-    color: #a0a0a0;
-    padding: 8px;
+    background: rgba(255, 255, 255, 0.4);
+    color: #333333;
+    padding: 6px;
     border-radius: 4px;
-    font-size: 12px;
+    font-size: 10px;
     font-weight: 300;
     line-height: 1.2;
     letter-spacing: 1.5px;
     white-space: nowrap;
     user-select: none;
     pointer-events: none;
-    opacity: 0.8;
+    opacity: 0.5;
   }
 `;
 
