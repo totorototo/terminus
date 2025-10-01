77f14d9c01c3af971dac04f21a2b83295a11601e
toto rototo <el.totorototo@gmail.com>
2025-10-01 14:26:20 +0200

    improve checkpoint behaviour

--- src/components/threeDimensionalProfile/ThreeDimensionalProfile.jsx
+++ src/components/threeDimensionalProfile/ThreeDimensionalProfile.jsx
@@ -1,8 +1,48 @@
-import React, { Fragment, useMemo } from "react";
+import React, { Fragment, useMemo, useRef, useState } from "react";
 import { scaleLinear } from "d3-scale";
 import ElevationProfile from "../elevationProfile/ElevationProfile";
 import { Html } from "@react-three/drei";
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 import style from "./ThreeDimensionalProfile.style.js";
+
+function Marker({ children, ...props }) {
+  const ref = useRef();
+  // This holds the local occluded state
+  const [isOccluded, setOccluded] = useState();
+  const [isInRange, setInRange] = useState();
+  const isVisible = isInRange && !isOccluded;
+
+  // Test distance
+  const vec = new THREE.Vector3();
+  useFrame((state) => {
+    const range =
+      state.camera.position.distanceTo(ref.current.getWorldPosition(vec)) <= 10;
+    if (range !== isInRange) setInRange(range);
+  });
+
+  return (
+    <group ref={ref}>
+      <Html
+        // 3D-transform contents
+        // transform
+        // Hide contents "behind" other meshes
+        occlude
+        // Tells us when contents are occluded (or not)
+        onOcclude={setOccluded}
+        // We just interpolate the visible state into css opacity and transforms
+        style={{
+          transition: "all 0.2s",
+          opacity: isVisible ? 1 : 0,
+          transform: `scale(${isVisible ? 1 : 0.25})`,
+        }}
+        {...props}
+      >
+        {children}
+      </Html>
+    </group>
+  );
+}
 
 function ThreeDimensionalProfile({
   coordinates,
@@ -153,13 +193,19 @@ function ThreeDimensionalProfile({
           checkpointsPoints3D &&
           checkpointsPoints3D.length > 0 &&
           checkpointsPoints3D.map((cp, index) => (
-            <Html
+            // <Html
+            //   key={index}
+            //   position={[cp.point3D[0], cp.point3D[1] + 0.2, cp.point3D[2]]}
+            //   style={{ pointerEvents: "none" }}
+            // >
+            //   <div className="checkpoint-label">{cp.name}</div>
+            // </Html>
+            <Marker
               key={index}
               position={[cp.point3D[0], cp.point3D[1] + 0.2, cp.point3D[2]]}
-              style={{ pointerEvents: "none" }}
             >
               <div className="checkpoint-label">{cp.name}</div>
-            </Html>
+            </Marker>
           ))}
       </Fragment>
     ))
