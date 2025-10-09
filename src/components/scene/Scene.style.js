3561066b808680b435b9d53d6e45ee22dfdc0941
toto rototo <el.totorototo@gmail.com>
2025-10-09 17:56:28 +0200

    clean up styles, use theme

--- src/components/scene/Scene.style.js
+++ src/components/scene/Scene.style.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 const style = (Component) => styled(Component)`
   .checkpoint-label {
-    background: rgba(255, 255, 255, 0.4);
-    color: #333333;
+    background: ${(props) => props.theme.colors.light["--color-text"]}66;
+    color: ${(props) => props.theme.colors.light["--color-background"]};
     padding: 6px;
     border-radius: 4px;
     font-size: 10px;
-    font-weight: 300;
+    font-weight: ${(props) => props.theme.font.weights["--font-weight-light"]};
     line-height: 1.2;
     letter-spacing: 1.5px;
     white-space: nowrap;
