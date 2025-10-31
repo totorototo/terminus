e1bb80122db2cd255f612a06908560b7eccb0e6a
toto rototo <el.totorototo@gmail.com>
2025-10-31 15:36:32 +0100

    refactor: update slope colors

--- src/components/elevationProfile/ElevationProfile.jsx
+++ src/components/elevationProfile/ElevationProfile.jsx
@@ -41,13 +41,13 @@ const createColorScale = () => {
 const getRange = (percent) => {
   if (Math.abs(percent) < 5) {
     return ELEVATION_GRADE.SMALL;
-  } else if (Math.abs(percent) >= 5 && Math.abs(percent) < 7) {
+  } else if (Math.abs(percent) >= 5 && Math.abs(percent) < 10) {
     return ELEVATION_GRADE.EASY;
-  } else if (Math.abs(percent) >= 7 && Math.abs(percent) < 10) {
-    return ELEVATION_GRADE.MEDIUM;
   } else if (Math.abs(percent) >= 10 && Math.abs(percent) < 15) {
+    return ELEVATION_GRADE.MEDIUM;
+  } else if (Math.abs(percent) >= 15 && Math.abs(percent) < 20) {
     return ELEVATION_GRADE.DIFFICULT;
-  } else if (Math.abs(percent) >= 15) {
+  } else if (Math.abs(percent) >= 20) {
     return ELEVATION_GRADE.HARD;
   }
   return ELEVATION_GRADE.UNKNOWN;
