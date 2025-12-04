1af282beb106dd277fb9aeec28a9aff66340e27f
toto rototo <el.totorototo@gmail.com>
2025-12-04 14:07:41 +0100

    refactor: update dependencies

--- vite-plugin-gpx.js
+++ vite-plugin-gpx.js
@@ -1,72 +1,69 @@
- import toGeoJSON from '@mapbox/togeojson'
-import { basename } from 'node:path'
-import { readFile } from 'node:fs/promises'
-import { DOMParser } from 'xmldom'
-
+import toGeoJSON from "@mapbox/togeojson";
+import { basename } from "node:path";
+import { readFile } from "node:fs/promises";
+import { DOMParser } from "@xmldom/xmldom";
 
 export function gpxPlugin() {
-	let basePath
-	const gpxPaths = new Map()
-	return {
-		name: 'vite-plugin-gpx',
-		configResolved(cfg) {
-			const viteConfig = cfg
-			basePath = (viteConfig.base?.replace(/\/$/, '') || '') + '/@gpx/'
-		},
-		async load(id) {
-			if (!/\.gpx$/.test(id)) return null
+  let basePath;
+  const gpxPaths = new Map();
+  return {
+    name: "vite-plugin-gpx",
+    configResolved(cfg) {
+      const viteConfig = cfg;
+      basePath = (viteConfig.base?.replace(/\/$/, "") || "") + "/@gpx/";
+    },
+    async load(id) {
+      if (!/\.gpx$/.test(id)) return null;
 
-			const srcURL = new URL(id, 'file://')
-			const fileContents = await readFile(decodeURIComponent(srcURL.pathname))
+      const srcURL = new URL(id, "file://");
+      const fileContents = await readFile(decodeURIComponent(srcURL.pathname));
 
-			gpxPaths.set(basename(srcURL.pathname), id)
+      gpxPaths.set(basename(srcURL.pathname), id);
 
-			let src
-			if (!this.meta.watchMode) {
-				const handle = this.emitFile({
-					name: basename(srcURL.pathname),
-					source: fileContents,
-					type: 'asset'
-				})
+      let src;
+      if (!this.meta.watchMode) {
+        const handle = this.emitFile({
+          name: basename(srcURL.pathname),
+          source: fileContents,
+          type: "asset",
+        });
 
-				src = `__VITE_ASSET__${handle}__`
-			} else {
-				src = basePath + basename(srcURL.pathname)
-			}
+        src = `__VITE_ASSET__${handle}__`;
+      } else {
+        src = basePath + basename(srcURL.pathname);
+      }
 
-			// Call processing code
-		    const xml = new DOMParser().parseFromString(fileContents.toString());
-		    const geoJSON = toGeoJSON.gpx(xml)
-	
-			return {
-				code: `export default ${JSON.stringify(geoJSON)}`
-			}
-		},
-		configureServer(server) {
-			server.middlewares.use(async (req, res, next) => {
-				if (req.url?.startsWith(basePath)) {
-					const [, id] = req.url.split(basePath)
+      // Call processing code
+      const xml = new DOMParser().parseFromString(fileContents.toString());
+      const geoJSON = toGeoJSON.gpx(xml);
 
-					const gpxPath = gpxPaths.get(id)
+      return {
+        code: `export default ${JSON.stringify(geoJSON)}`,
+      };
+    },
+    configureServer(server) {
+      server.middlewares.use(async (req, res, next) => {
+        if (req.url?.startsWith(basePath)) {
+          const [, id] = req.url.split(basePath);
 
-					if (!gpxPath)
-						throw new Error(
-							`gpx cannot find GPX file with id "${id}" this is likely an internal error. Files are ${JSON.stringify(
-								gpxPaths
-							)}`
-						)
+          const gpxPath = gpxPaths.get(id);
 
-					res.setHeader('Content-Type', 'application/gpx+xml')
-					res.setHeader('Cache-Control', 'max-age=360000')
-					const buffer = await readFile(gpxPath)
-					const contents = buffer.toString()
-					return res.end(contents)
-				}
+          if (!gpxPath)
+            throw new Error(
+              `gpx cannot find GPX file with id "${id}" this is likely an internal error. Files are ${JSON.stringify(
+                gpxPaths,
+              )}`,
+            );
 
-				next()
-			})
-		}
-	}
+          res.setHeader("Content-Type", "application/gpx+xml");
+          res.setHeader("Cache-Control", "max-age=360000");
+          const buffer = await readFile(gpxPath);
+          const contents = buffer.toString();
+          return res.end(contents);
+        }
+
+        next();
+      });
+    },
+  };
 }
-
-
